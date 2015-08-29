20831ea11f310a62c82a4c702f1f98a02dc9e3ba
Marta Stefanyshyn <martaolehivna@gmail.com>
2015-08-29 13:43:13 -0700

    Devise user and Category entity

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+	
+	def main
+	end
+
+	def contact
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :categories
+  devise_for :users
+  root 'static_pages#main'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
